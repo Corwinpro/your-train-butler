bc5a00ff0060fde28db3d53b624279764a001f2f
corwinpro <corwinat@gmail.com>
2021-12-21 22:36:57 +0000

    better logging

--- rail_bot/bot/job_recovery.py
+++ rail_bot/bot/job_recovery.py
@@ -11,6 +11,10 @@ logger = logging.getLogger(__name__)
 def recover_jobs(job_queue: JobQueue):
     job_service = create_job_service()
     active_jobs = job_service.get_jobs()
+    logger.info(
+        f"Found {len(active_jobs)} active jobs that will be recovered. "
+        f"{[(job.chat_id, job.origin, job.destination, job.time) for job in active_jobs]}"
+    )
 
     for job in active_jobs:
         response = subscribe_departure(
