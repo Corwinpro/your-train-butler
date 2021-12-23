5516a29f0eb1d3b234d7b93a4964000b577a86e0
corwinpro <corwinat@gmail.com>
2021-12-23 19:29:06 +0000

    another try to format html

--- rail_bot/bot/help_handler.py
+++ rail_bot/bot/help_handler.py
@@ -3,23 +3,18 @@ from telegram import Update
 
 
 def help(update: Update, context: CallbackContext):
-    text = """
-    <p>Train Check Bot 🚂</p>
-    Train Check Bot can:
-    <ul>
-        <li> Tell you about live departures from a station:
-
-            <blockquote>/board KGX</blockquote>
-
-    or between stations:
-
-        <blockquote>/board KGX CBG</blockquote>
-
-    <li> Notify you about service disruptions. To recieve notifications in case
-    anything goes wrong, subscribe to a service update via:
-
-            <blockquote>/subscribe KGX CBG 12:23</blockquote>
-    """
+    text = (
+        "<b>Train Check Bot</b>🚂\n"
+        "Train Check Bot can:\n"
+        "- Tell you about live departures from a station:\n"
+        "    <code>/board KGX</code>\n"
+        "  or between stations:\n"
+        "    <code>/board KGX CBG</code>\n"
+        "- Notify you about service disruptions. "
+        "To recieve notifications in case anything goes wrong, "
+        "subscribe to a service update via\n"
+        "  <code>/subscribe KGX CBG 12:23</code>"
+    )
     update.message.reply_html(text)
 
 
