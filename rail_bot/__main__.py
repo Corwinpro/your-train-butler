859b7bd253e539583d107be399c7d0e91621f14e
corwinpro <corwinat@gmail.com>
2021-12-25 15:14:07 +0000

    WIP: playing with buttons

--- rail_bot/__main__.py
+++ rail_bot/__main__.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from telegram.callbackquery import CallbackQuery
 
 from telegram.ext import Updater
 
@@ -31,30 +32,33 @@ TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "")
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 
+
+keyboard = [
+    [
+        InlineKeyboardButton("Option 1", callback_data='1'),
+        InlineKeyboardButton("Option 2", callback_data='2'),
+    ],
+    [InlineKeyboardButton("Option 3", callback_data='3')],
+]
+
+reply_markup = InlineKeyboardMarkup(keyboard)
+
 def test_but(update: Update, context: CallbackContext) -> None:
     """Sends a message with three inline buttons attached."""
-    keyboard = [
-        [
-            InlineKeyboardButton("Option 1", callback_data='1'),
-            InlineKeyboardButton("Option 2", callback_data='2'),
-        ],
-        [InlineKeyboardButton("Option 3", callback_data='3')],
-    ]
 
-    reply_markup = InlineKeyboardMarkup(keyboard)
 
     update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
 
 def button(update: Update, context: CallbackContext) -> None:
     """Parses the CallbackQuery and updates the message text."""
-    query = update.callback_query
+    query: CallbackQuery = update.callback_query
 
     # CallbackQueries need to be answered, even if no notification to the user is needed
     # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     query.answer()
 
-    query.edit_message_text(text=f"Selected option: {query.data}")
+    query.edit_message_text(text=f"Selected option: {query.data}", reply_markup=reply_markup)
 
 def main():
     updater = Updater(token=TELEGRAM_TOKEN)
