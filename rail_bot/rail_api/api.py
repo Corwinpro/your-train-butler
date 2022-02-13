8e45faba0febffb27c23150cc67fd7abd9da3edf
corwinpro <corwinat@gmail.com>
2022-02-13 23:08:17 +0000

    improve travel info response parsing

--- rail_bot/rail_api/api.py
+++ rail_bot/rail_api/api.py
@@ -1,11 +1,15 @@
+import logging
 import os
 from typing import Optional
 
-from zeep import xsd
+from zeep import helpers, xsd
 from zeep.client import Client
 from zeep.plugins import HistoryPlugin
 
-from rail_bot.rail_api.travel import Travel
+from rail_bot.rail_api.travel import Travel, ON_TIME_LABEL
+
+logger = logging.getLogger(__name__)
+
 
 LDB_TOKEN = os.environ.get("LDB_TOKEN", "")
 WSDL = "http://lite.realtime.nationalrail.co.uk/OpenLDBWS/wsdl.aspx?ver=2017-10-01"
@@ -25,11 +29,10 @@ header = xsd.Element(
 )
 header_value = header(TokenValue=LDB_TOKEN)
 
-# Standard estimated departure time label string as returned by the LDB service
-ON_TIME_LABEL = "On time"
 
-
-def departure_board(from_station: str, to_station: Optional[str], rows: Optional[int]=None):
+def departure_board(
+    from_station: str, to_station: Optional[str], rows: Optional[int] = None
+):
     from_station = from_station.upper()
 
     if to_station is not None:
@@ -77,15 +80,16 @@ def departure_board(from_station: str, to_station: Optional[str], rows: Optional
 def next_departure_status(
     from_station: str, to_station: str, timeOffset: int = 0
 ) -> Optional[Travel]:
-    res = client.service.GetNextDepartures(
+    res = client.service.GetNextDeparturesWithDetails(
         crs=from_station.upper(),
         filterList=[to_station.upper()],
         timeOffset=timeOffset,
         _soapheaders=[header_value],
     )
-    departure_service = res.departures.destination[0].service
+    response = helpers.serialize_object(res, dict)
     try:
-        travel = Travel.from_departure_service(departure_service)
-    except AttributeError:
+        travel = Travel.from_response(response)
+    except Exception as e:
         travel = None
+        logger.warning(f"Failed to create Travel object from response {response}: {e}")
     return travel
--- rail_bot/rail_api/tests/test_travel.py
+++ rail_bot/rail_api/tests/test_travel.py
@@ -1,6 +1,17 @@
+import datetime
+import json
+import pkg_resources
 import unittest
 
 from rail_bot.rail_api.travel import Travel
+
+
+EXAMPLE_RESPONSE = pkg_resources.resource_filename(
+    "rail_bot.rail_api.tests", "resources/response.json"
+)
+EXAMPLE_DELAYED_RESPONSE = pkg_resources.resource_filename(
+    "rail_bot.rail_api.tests", "resources/response_delayed.json"
+)
 
 
 class TestTravel(unittest.TestCase):
@@ -8,12 +19,12 @@ class TestTravel(unittest.TestCase):
         travel_1 = Travel(
             origin="kgx",
             destination="cbg",
-            scheduled_departure="12:23",
+            scheduled_departure=datetime.time(12, 23),
             service_type="train",
             delay_info=None,
             cancel_info=None,
-            scheduled_arrival="12:10",
-            estimated_arrival=None,
+            scheduled_arrival=datetime.time(12, 10),
+            estimated_arrival=datetime.time(13, 10),
             estimated_departure="On time",
         )
 
@@ -22,12 +33,12 @@ class TestTravel(unittest.TestCase):
         travel_2 = Travel(
             origin="kgx",
             destination="cbg",
-            scheduled_departure="12:23",
+            scheduled_departure=datetime.time(12, 23),
             service_type="train",
             delay_info=None,
             cancel_info=None,
-            scheduled_arrival="12:10",
-            estimated_arrival=None,
+            scheduled_arrival=datetime.time(12, 10),
+            estimated_arrival=datetime.time(13, 10),
             estimated_departure="On time",
         )
 
@@ -38,23 +49,23 @@ class TestTravel(unittest.TestCase):
         travel_1 = Travel(
             origin="kgx",
             destination="cbg",
-            scheduled_departure="12:23",
+            scheduled_departure=datetime.time(12, 23),
             service_type="train",
             delay_info=None,
             cancel_info=None,
-            scheduled_arrival="12:10",
-            estimated_arrival=None,
+            scheduled_arrival=datetime.time(12, 10),
+            estimated_arrival=datetime.time(13, 10),
             estimated_departure="On time",
         )
         travel_2 = Travel(
             origin="cbg",
             destination="kgx",
-            scheduled_departure="12:23",
+            scheduled_departure=datetime.time(12, 23),
             service_type="train",
             delay_info=None,
             cancel_info=None,
-            scheduled_arrival="12:10",
-            estimated_arrival=None,
+            scheduled_arrival=datetime.time(12, 10),
+            estimated_arrival=datetime.time(13, 10),
             estimated_departure="On time",
         )
 
@@ -64,13 +75,13 @@ class TestTravel(unittest.TestCase):
         travel_3 = Travel(
             origin="kgx",
             destination="cbg",
-            scheduled_departure="12:23",
+            scheduled_departure=datetime.time(12, 23),
             service_type="train",
             delay_info=None,
             cancel_info=None,
-            scheduled_arrival="12:10",
-            estimated_arrival=None,
-            estimated_departure="12:23",
+            scheduled_arrival=datetime.time(12, 10),
+            estimated_arrival=datetime.time(13, 10),
+            estimated_departure=datetime.time(12, 23),
         )
 
         self.assertTrue(travel_1 != travel_3)
@@ -79,17 +90,51 @@ class TestTravel(unittest.TestCase):
         travel_4 = Travel(
             origin="kgx",
             destination="cbg",
-            scheduled_departure="12:23",
+            scheduled_departure=datetime.time(12, 23),
             service_type="train",
             delay_info=None,
             cancel_info=None,
-            scheduled_arrival="12:10",
-            estimated_arrival="12:10",
+            scheduled_arrival=datetime.time(12, 10),
+            estimated_arrival=datetime.time(12, 10),
             estimated_departure="On time",
         )
 
         self.assertTrue(travel_1 != travel_4)
         self.assertTrue(travel_4 != travel_1)
+
+    def test_travel_from_response(self):
+        with open(EXAMPLE_RESPONSE) as f:
+            response = json.load(f)
+
+        travel = Travel.from_response(response)
+
+        self.assertEqual(travel.origin, "Flowery Field")
+        self.assertEqual(travel.destination, "Dinting")
+        self.assertEqual(travel.scheduled_departure, datetime.time(19, 46))
+        self.assertEqual(travel.estimated_departure, datetime.time(19, 46))
+        self.assertEqual(travel.scheduled_arrival, datetime.time(20, 0))
+        self.assertEqual(travel.estimated_arrival, datetime.time(20, 0))
+        self.assertEqual(travel.service_type, "train")
+
+        self.assertFalse(travel.is_delayed)
+        self.assertFalse(travel.is_cancelled)
+
+    def test_travel_from_response_delayed(self):
+        with open(EXAMPLE_DELAYED_RESPONSE) as f:
+            response = json.load(f)
+
+        travel = Travel.from_response(response)
+
+        self.assertEqual(travel.origin, "Flowery Field")
+        self.assertEqual(travel.destination, "Dinting")
+        self.assertEqual(travel.scheduled_departure, datetime.time(21, 14))
+        self.assertEqual(travel.estimated_departure, datetime.time(21, 31))
+        self.assertEqual(travel.scheduled_arrival, datetime.time(21, 28))
+        self.assertEqual(travel.estimated_arrival, datetime.time(21, 43))
+        self.assertEqual(travel.service_type, "train")
+
+        self.assertTrue(travel.is_delayed)
+        self.assertFalse(travel.is_cancelled)
 
 
 if __name__ == "__main__":
